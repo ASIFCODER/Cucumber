4e6bc45bf5d4586c307e5a8121c93184c1741b55
ASIFCODER <mohaideenasif90@gmail.com>
2022-05-21 18:33:59 +0530

    added

---
+++ src/selenium/Test.java
@@ -0,0 +1,37 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test {
+public static void main(String[] args) {
+	
+	
+	System.setProperty("webdrive.chorom.drive","C:\\Users\\User\\eclipse-workspace\\Selenium\\drive\\chromedriver.exe");
+	
+WebDriver driver = new ChromeDriver();	
+	
+	
+	
+	
+	
+	
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/seleniumPractice/Selenium.java
@@ -0,0 +1,9 @@
+package seleniumPractice;
+
+public class Selenium {
+	
+	
+	
+	
+
+}
